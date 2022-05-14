f97fe4da17b20a469e520795d09d7b2bbd566e52
aparna715 <87603519+aparna715@users.noreply.github.com>
2022-05-14 19:10:26 +0530

    Create Day5 majority element.cpp

---
+++ Day5 majority element.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        unordered_map<int,int>cnt;
+        for(int num:nums){
+            if(++cnt[num]>nums.size()/2)
+                return num;
+        }
+        return 0;
+    }
+};
